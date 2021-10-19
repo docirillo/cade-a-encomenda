a9c4ae6c2b8210bf5966c62d4a52a716ee8056ee
Daniel Cirillo <sapremo@outlook.com>
2021-10-18 22:09:37 -0300

    correção home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import Layout from '../components/template/Layout';
 import useAppData from '../data/hook/useAppData';
 
 export default function Home() {
-  const { changeTheme } = useAppData();
   return (
     <Layout
       title="Página Inicial"
       subtitle="Estamos construindo uma aplicação de encomendas"
     >
       <h3>Conteúdo</h3>
-      <button onClick={changeTheme}>Change Theme</button>
     </Layout>
   );
 }
