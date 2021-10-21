3e77a083d0375a5bb7d987d3b04eba5e9c050bb3
Daniel Cirillo <sapremo@outlook.com>
2021-10-20 21:44:57 -0300

    inclusão avatar e profile

---
+++ src/components/template/UserAvatar.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import useAuth from '../../data/hook/useAuth';
+
+interface UserAvatarProps {
+  className?: string;
+}
+
+export default function UserAvatar(props: UserAvatarProps) {
+  const { user } = useAuth();
+
+  return (
+    <Link href="/profile">
+      <img
+        src={user?.imageUrl ?? '/images/avatar.svg'}
+        alt="User Avatar"
+        className={`h-10 w-10 rounded-full cursor-pointer ${props.className}`}
+      />
+    </Link>
+  );
+}
---
+++ src/pages/profile.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/template/Layout';
+
+export default function Profile() {
+  return (
+    <Layout
+      title="Profile"
+      subtitle="Estamos construindo uma aplicação de encomendas"
+    >
+      <h1>Profile</h1>
+    </Layout>
+  );
+}
