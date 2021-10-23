f57e7880481dfcbaa8a4ac7680350f255762a2c3
Daniel Cirillo <sapremo@outlook.com>
2021-10-23 17:50:55 -0300

    localstorage for theme

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,18 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
-type Theme = 'dark' | '';
+//type Theme = 'dark' | '';
 
 interface AppContextProps {
   theme?: string;
   changeTheme?: () => void;
 }
-const AppContext = createContext({});
+const AppContext = createContext<AppContextProps>({});
 
 export function AppProvider(props) {
-  const [theme, setTheme] = useState<Theme>('dark');
+  const [theme, setTheme] = useState('dark');
+
   function changeTheme() {
+    const newTheme = theme === '' ? 'dark' : '';
     setTheme(theme === '' ? 'dark' : '');
+    localStorage.setItem('theme', newTheme);
   }
+
+  //salvando tema preferido no localstorage
+  useEffect(() => {
+    const themeSaved = localStorage.getItem('theme');
+    setTheme(themeSaved);
+  }, []);
+
   return (
     <AppContext.Provider value={{ theme, changeTheme }}>
       {props.children}
