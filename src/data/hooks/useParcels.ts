7b99b2a83b033eebf2ca96919d566cdc1a5ceca2
Daniel Cirillo <sapremo@outlook.com>
2021-10-24 17:54:34 -0300

    crud com hooks

---
+++ src/data/hooks/useParcels.ts
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import Parcel from '../../core/Parcel';
+import ParcelRepository from '../../core/ParcelRepository';
+import ParcelCollection from '../../db/ParcelCollection';
+import useTableOrForm from './useTableorForm';
+
+export default function useParcels() {
+  const repo: ParcelRepository = new ParcelCollection();
+  const { tableVisible, showTable, showForm } = useTableOrForm();
+
+  const [parcel, setParcel] = useState(Parcel.emptyParcel());
+  const [parcels, setParcels] = useState<Parcel[]>([]);
+
+  useEffect(() => {
+    repo.getAll().then(setParcels);
+  }, []);
+
+  //funcao para obter todos e deixar tabela visivel
+  function getAll() {
+    repo.getAll().then((parcels) => {
+      setParcels(parcels);
+      showTable();
+    });
+  }
+
+  //funcao para atualizar parcel e deixar form visivel
+  function getParcel(parcel: Parcel) {
+    setParcel(parcel);
+    showForm();
+  }
+
+  //funcao para apagar Parcel
+  async function deleteParcel(parcel: Parcel) {
+    await repo.delete(parcel);
+    getAll();
+  }
+
+  //funcao para criar novo Parcel
+  function newParcel() {
+    setParcel(Parcel.emptyParcel());
+    showForm();
+  }
+
+  //funcao para salvar Parcel
+  async function saveParcel(parcel: Parcel) {
+    await repo.save(parcel);
+    getAll();
+  }
+
+  return {
+    parcel,
+    parcels,
+    newParcel,
+    saveParcel,
+    deleteParcel,
+    getParcel,
+    getAll,
+    tableVisible,
+    showTable,
+  };
+}
--- src/pages/parcels.tsx
+++ src/pages/parcels.tsx
@@ -1,61 +1,27 @@
-import { useEffect, useState } from 'react';
 import NewParcelBtn from '../components/parcel/NewParcelBtn';
 import NewParcelForm from '../components/parcel/NewParcelForm';
 import ParcelTable from '../components/parcel/ParcelTable';
 import Layout from '../components/template/Layout';
-import Parcel from '../core/Parcel';
-import ParcelRepository from '../core/ParcelRepository';
-import ParcelCollection from '../db/ParcelCollection';
+import useParcels from '../data/hooks/useParcels';
 
 export default function Parcels() {
-  const repo: ParcelRepository = new ParcelCollection();
-
-  const [parcel, setParcel] = useState(Parcel.emptyParcel());
-  const [parcels, setParcels] = useState<Parcel[]>([]);
-  const [visible, setVisible] = useState<'table' | 'form'>('table');
-
-  useEffect(() => {
-    repo.getAll().then(setParcels);
-  }, []);
-
-  //funcao para obter todos e deixar tabela visivel
-  function getAll() {
-    repo.getAll().then((parcels) => {
-      setParcels(parcels);
-      setVisible('table');
-    });
-  }
-
-  //funcao para atualizar parcel e deixar form visivel
-  function parcelSelected(parcel: Parcel) {
-    setParcel(parcel);
-    setVisible('form');
-  }
-
-  //funcao para apagar Parcel
-  async function parcelDeleted(parcel: Parcel) {
-    await repo.delete(parcel);
-    getAll();
-  }
-
-  //funcao para criar novo Parcel
-  function newParcel(parcel: Parcel) {
-    setParcel(Parcel.emptyParcel());
-    setVisible('form');
-  }
-
-  //funcao para salvar Parcel
-  async function saveParcel(parcel: Parcel) {
-    await repo.save(parcel);
-    getAll();
-  }
+  const {
+    parcel,
+    parcels,
+    newParcel,
+    getParcel,
+    deleteParcel,
+    saveParcel,
+    tableVisible,
+    showTable,
+  } = useParcels();
 
   return (
     <Layout
       title="Parcels"
       subtitle="Estamos construindo uma aplicação de encomendas"
     >
-      {visible === 'table' ? (
+      {tableVisible ? (
         <>
           <div className="flex justify-end">
             <NewParcelBtn className="mb-4 bg-yellow-500" onClick={newParcel}>
@@ -65,15 +31,15 @@ export default function Parcels() {
 
           <ParcelTable
             parcels={parcels}
-            parcelSelected={parcelSelected}
-            parcelDeleted={parcelDeleted}
+            parcelSelected={getParcel}
+            parcelDeleted={deleteParcel}
           />
         </>
       ) : (
         <NewParcelForm
           parcel={parcel}
           parcelChanged={saveParcel}
-          cancelled={() => setVisible('table')}
+          cancelled={showTable}
         />
       )}
     </Layout>
