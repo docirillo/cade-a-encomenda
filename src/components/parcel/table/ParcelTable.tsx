c904cf0ab3e5f373c6fddcb2a38da7fc891e49f2
Daniel Cirillo <sapremo@outlook.com>
2021-10-24 22:33:43 -0300

    ajuste de cores

--- src/components/parcel/table/ParcelTable.tsx
+++ src/components/parcel/table/ParcelTable.tsx
@@ -148,7 +148,7 @@ export default function ParcelTable(props) {
   //montagem da tabela
   return (
     <div className="flex flex-col">
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-6">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="w-full divide-y divide-gray-200">
