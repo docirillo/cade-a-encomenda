901f086f70537a10c7b9ac397b42d63a4b0854dd
Daniel Cirillo <sapremo@outlook.com>
2021-10-26 10:10:47 -0300

    ajuste icones

--- src/components/template/ChangeThemeButton.tsx
+++ src/components/template/ChangeThemeButton.tsx
@@ -14,10 +14,10 @@ export default function ChangeThemeButton(props: ChangeThemeButtonProps) {
       <div
         className={`flex items-center justify-center bg-white text-yellow-600 w-6 h-6 rounded-full`}
       >
-        {IconSun(5)}
+        {IconSun(8)}
       </div>
       <div className={`hidden lg:flex items-center ml-4 text-white`}>
-        <span className={`text-sm`}>Light</span>
+        <span className={`text-sm`}>Dia</span>
       </div>
     </div>
   ) : (
@@ -26,12 +26,12 @@ export default function ChangeThemeButton(props: ChangeThemeButtonProps) {
       className={`hidden sm:flex items-center justify-end bg-gradient-to-r from-gray-500 to-gray-900 w-14 lg:w-24 h-8 cursor-pointer p-1 rounded-full`}
     >
       <div className={`hidden lg:flex items-center mr-2 text-gray-300`}>
-        <span className={`text-sm`}>Dark</span>
+        <span className={`text-sm`}>Noite</span>
       </div>
       <div
         className={`flex items-center justify-center bg-black text-blue-300 w-6 h-6 rounded-full`}
       >
-        {IconMoon(5)}
+        {IconMoon(8)}
       </div>
     </div>
   );
